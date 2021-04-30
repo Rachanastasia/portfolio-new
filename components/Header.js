d5f5eca1a89c2768f5e83aa77468a48ac2c171cb
Rachel A Reilly <R.a.reill18@gmail.com>
2021-04-30 12:17:48 -0500

    Remove boilerplate and set up major components

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header(){
+//This component contains the typing animation
+//in the previous portfolio version
+    return (
+        <header>
+            <h1>This is my header</h1>
+        </header>
+    )
+}
---
+++ components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Projects(){
+
+    return (
+        <section>
+            <h2>This is the projects section</h2>
+        </section>
+    )
+}
