ae5a619d637b6fcf302f251ab735ee1e5abc65fa
Rachel Reilly <r.a.reill18@gmail.com>
2021-07-24 18:28:21 -0500

    adds colors to animation

--- components/Banner/LineOfText.js
+++ components/Banner/LineOfText.js
@@ -26,16 +26,15 @@ function FullLine({tabs, children}){
 export function Line19({tabs=1}){
     return (
        <FullLine tabs={tabs}>
-            <Word word='for' color='one' />
-            <Word word='( ' color='one' />
-            <Word word='let' color='two'  />
-            <Character char='i' />
-            <Character char='=' />
-            <Character char='0' />
+            <Word word='for' color='three' />
+            <Word word='( ' color='two' />
+            <Character char='i' color='two'  />
+            <Character char='=' color='one'/>
+            <Character char='0' color='one'/>
             <Character char=';' hasSpace={false}/>
             <Character char='i' />
             <Word word='<=' color='one' />
-            <Word word='input.' color='one' />
+            <Word word='input.' color='two' />
             <Word word='length' color='one' hasSpace={false}/>
             <Character char=';' hasSpace={false}/>
             <Character char='i' />
@@ -49,11 +48,11 @@ export function Line19({tabs=1}){
 export function Line20({tabs=1}){
     return (
        <FullLine tabs={tabs}>
-            <Word word='const' color='one' />
+            <Word word='const' color='three' />
             <Word word='currentChar' color='two' />
-            <Word word='=' color='two' />
+            <Word word='=' color='one' />
             <Word word='input[i]?.' color='two' />
-            <Word word='toLowerCase' color='two' hasSpace={false} />
+            <Word word='toLowerCase' color='four' hasSpace={false} />
             <Word word='( )' color='two' hasSpace={false}/>
         </FullLine>
     )
@@ -62,15 +61,15 @@ export function Line20({tabs=1}){
 export function Line21({tabs=1}){
     return (
        <FullLine tabs={tabs}>
-            <Word word='if' color='one' />
+            <Word word='if' color='three' />
             <Character char='(' />
             <Word word='currentChar' color='two' />
-            <Word word='===' color='two' />
+            <Word word='===' color='one' />
             <Word word="' '" color='two' />
-            <Word word='||' color='two' />
+            <Word word='||' color='one' />
             <Word word='currentChar' color='two' />
 
-            <Word word='===' color='two' />
+            <Word word='===' color='one' />
             <Word word="'\n'" color='two' />
             <Character char=')' hasSpace={false}/>
             <Character char='{' />
@@ -80,7 +79,7 @@ export function Line21({tabs=1}){
 export function Line22({tabs}){
     return (
         <FullLine tabs={tabs}>
-            <Word word='validateIngredientAndAddToDataIngredients' color='two' />
+            <Word word='validateIngredientAndAddToDataIngredients' color='four' />
             <Word word=' ( )' color='two' hasSpace={false} />
         </FullLine>
     )
@@ -88,7 +87,7 @@ export function Line22({tabs}){
 export function Line23({tabs}){
     return (
         <FullLine tabs={tabs}>
-            <Word word='sortCurrentWord' color='two' />
+            <Word word='sortCurrentWord' color='four' />
             <Word word=' ( )' color='two' hasSpace={false} />
         </FullLine>
     )
@@ -97,7 +96,7 @@ export function Line24({tabs}){
     return (
         <FullLine tabs={tabs}>
             <Word word='currentWord' color='two' />
-            <Character char='=' />
+            <Character char='=' color='one'/>
             <Character char="' '"/>
         </FullLine>
     )
@@ -107,7 +106,7 @@ export function Line25({tabs}){
     return (
         <FullLine tabs={tabs}>
             <Character char='}' />
-            <Word word='else' color='two' />
+            <Word word='else' color='three' />
             <Character char='{' />
         </FullLine>
     )
@@ -116,11 +115,11 @@ export function Line25({tabs}){
 export function Line26({tabs=1}){
     return (
        <FullLine tabs={tabs}>
-            <Word word='currentWord' color='one' />
-            <Character char='=' />
             <Word word='currentWord' color='two' />
-            <Character char='+' />
-            <Word word='currentChar' color='one' />
+            <Character char='=' color='one' />
+            <Word word='currentWord' color='two' />
+            <Character char='+' color='one'/>
+            <Word word='currentChar' color='two' />
         </FullLine>
     )
 }
@@ -144,6 +143,6 @@ function Word({word, color, hasSpace=true}){
         <span className={`animation-${color} animated-letter`} key={index}>{letter}</span>)}</div>
 }
 
-function Character({char, color='one', hasSpace=true}){
+function Character({char, color='two', hasSpace=true}){
     return <span className={`animation-${color} animated-letter ${hasSpace && 'animated-word'}`}>{char}</span>
 }
