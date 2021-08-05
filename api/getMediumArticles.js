33a64a2df2e06c56f8c87770b810df567be895eb
Rachel Reilly <R.a.reill18@gmail.com>
2021-08-04 20:24:49 -0500

    parses content in feed and returns array

--- api/getMediumArticles.js
+++ api/getMediumArticles.js
@@ -3,9 +3,20 @@ let parser = new Parser();
 
 module.exports = {
     getMediumArticles: async () => {
-    const url = "https://medium.com/feed/@rachelrly"
-    const feed = await parser.parseURL(url)
-    // const test = await fetch(url, {encoding: null})
-    console.log('THIS IS THE XML', feed)
-    return feed
+    const MEDIUM_FEED = "https://medium.com/feed/@rachelrly"
+    const feed = await parser.parseURL(MEDIUM_FEED)
+    const parsedFeed = feed.items.map(parseArticle)
+    return parsedFeed
 }}
+
+function parseArticle(article){
+    const articleBody = article['content:encoded']
+    const date = new Date(article.isoDate)
+    const datePosted = date.toDateString()
+    return {
+        title: article.title,
+        link: article.link,
+        content: articleBody,
+        datePosted
+    }
+}
