d10dc59dbfc89fed3cf2585717f20cf1a7a7bea0
Rachel <r.a.reill18@gmail.com>
2021-08-25 15:14:21 -0500

    adds new colors and changes base styles for theme

--- client/components/Header.js
+++ client/components/Header.js
@@ -6,11 +6,11 @@ export default function Header(){
         <header>
             <h1 className='section-title main-title'>Rachel Reilly</h1>
             <div className='header-title-link-wrapper'>
-            <BaseButtonLink url='/projects' content='Projects'/>
-<BaseButtonLink url='/about' content='About'/>
-<BaseButtonLink url='/contact' content='Contact'/>
-<BaseButtonLink url='/blog' content='Blog'/>
-<BaseButtonLink url='/hire-me' content='Hire Me'/> 
+                <BaseButtonLink url='/projects' content='Projects'/>
+                <BaseButtonLink url='/about' content='About'/>
+                <BaseButtonLink url='/contact' content='Contact'/>
+                <BaseButtonLink url='/blog' content='Blog'/>
+                <BaseButtonLink url='/hire-me' content='Hire Me'/> 
             </div>
         </header>
     )
