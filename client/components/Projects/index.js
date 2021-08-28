3fcbd9340bc28fa6c7dedef5f4f9514085af7d6e
Rachel <r.a.reill18@gmail.com>
2021-08-28 11:39:27 -0500

    displays projects from object instead of array

--- client/components/Projects/index.js
+++ client/components/Projects/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import Image from 'next/image'
 import ProjectCardLandscape from './ProjectCardLandscape'
 import ProjectCardPortrait from './ProjectCardPortrait'
 
-import Sliced from '../../assets/scale-down.jpg'
+import SlicedNewImage from '../../assets/sliced-new.jpg'
+import SlicedOldImage from '../../assets/sliced-old-input-view.png'
 
 
-const PROJECTS = [
-    {
+const PROJECTS = {
+    SLICED_NEW: {
         title: 'Sliced',
         description: 'A recipe app that scales up and down ingredients',
         stack: ['React', 'Node.js', 'Express', 'CSS3', 'PostgreSQL'],
-        images: [Sliced],
+        image: SlicedNewImage,
         links: [
             {title: 'View App', href: 'https://sliced.rachanastasia.vercel.app/'},
             {title: 'Client Code', href: 'https://github.com/rachelrly/sliced-client'},
@@ -20,7 +20,20 @@ const PROJECTS = [
         ],
         date: 'October 2020 - Present'
     },
-    {
+    SLICED_OLD: {
+        title: 'Sliced',
+        description: 'A recipe app that scales up and down ingredients',
+        stack: ['React', 'Node.js', 'Express', 'CSS3', 'PostgreSQL'],
+        image: SlicedOldImage,
+        links: [
+            {title: 'View App', href: 'https://sliced.rachanastasia.vercel.app/'},
+            {title: 'Client Code', href: 'https://github.com/rachelrly/sliced-client'},
+            {title: 'Server Code', href: 'https://github.com/rachelrly/sliced-API'},
+            {title: 'Blog Post', href: 'https://rachelrly.medium.com/a-tale-of-three-string-parsing-algorithms-ed62a13bc62b'}
+        ],
+        date: 'October 2020 - Present'
+    },
+    SPACED_REPETITION: {
         title: 'Spaced Repetition',
         description: 'A language learning app that uses the spaced repetition technique',
         stack: ['React', 'Node.js', 'Express', 'CSS3', 'PostgreSQL'],
@@ -32,7 +45,7 @@ const PROJECTS = [
         ],
         date: 'October 2020 - November 2020'
     },
-    {
+    BOOKSHELF:{
         title: 'GraphQL Bookshelf',
         description: "This full-stack project explores types and the connections between data in GraphQL",
         stack: ['React', 'GraphQL', 'Apollo Client', 'Node.js', 'Express', 'CSS3'],
@@ -43,7 +56,7 @@ const PROJECTS = [
         ],
         date: 'January 2021'
     },
-    {
+    ONE_DIMENSIONAL_CHESS: {
         title: 'One Dimensional Chess',
         description: "One Dimensional Chess is a static two-player game that follows Glimne's rules for the game",
         stack: ['React', 'CSS3'],
@@ -53,7 +66,7 @@ const PROJECTS = [
         ],
         date: 'October 2020 - January 2021'
     },
-    {
+    LOAN_CALCULATOR: {
         title: 'Loan Calculator',
         description: 'A loan calculator that uses a reducer for state management',
         stack: ['React', 'CSS3'],
@@ -64,7 +77,7 @@ const PROJECTS = [
         ],
         date: 'February 2021'
     },
-    {
+    INIT: {
         title: 'Init',
         description: "Init is a social media platform for developers to connect over their work. Init was created by Rachel Reilly, Adyceum Magna Ccarri, Trevor J Alt, and Steven Henderson for the final capstone at Thinkful's Engineering Immersion program.",
         stack: ['React', 'Node.js', 'Express', 'CSS3', 'PostgreSQL'],
@@ -75,26 +88,15 @@ const PROJECTS = [
         ],
         date: 'November 2020 - December 2020'
     }
-]
+}
 
 export default function Projects(){
 
     return (
         <div>
             <h2 className='section-title'>Projects</h2>
-        
-            <div className='project1 project'>
-                <Image  src={Sliced}/>
-            </div>
-            <div className='project2 project'>
-
-            </div>
-            <div className='project3 project'>
-
-            </div>
-            <div className='project4 project'>
-
-            </div>
+            <ProjectCardLandscape {...PROJECTS.SLICED_NEW}/>
+            <ProjectCardPortrait {...PROJECTS.SLICED_OLD}/>
         </div>
     )
 }
