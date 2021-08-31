1708385a0fc935e7d5cd6f7cf42f72c004319783
Rachel <r.a.reill18@gmail.com>
2021-08-31 09:09:33 -0500

    sets up basic contact form

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -4,20 +4,25 @@ import SocialLinks  from './SocialLinks'
 
 export default function Contact(){
     return (
-        <section className='about-wrapper'> 
+        <section className='contact-wrapper'> 
             <h2>Contact</h2>
-            <div className='about-content-wrapper'>
-                {/* <img className='about-img' src={require('../assets/rachel.jpg')} /> */}
-                <div className="about-paragraph-wrapper">
-                    <p>
-                        Hi, I’m Rachel. I’m a Full Stack Developer from Chicago.
-                    </p>
-                    <p>
-                        My background as an artist informs my programming beyond the UI. I learned how to take concepts -- classes and context, hue and negative space -- and apply them for different effects. I practiced listening to feedback during critiques, realizing that other people often saw things in my work that I had overlooked. 
-                    </p>
-                    <p>In art school, I took classes in drawing, ceramics, and color theory to push my skills as an artist beyond my preferred discipline. I try to adapt this mindset when growing my abilities as a developer, practicing data structures and algorithms in Python, doing SQL drills, and diving into computer architecture, in addition to the daily work I do as a Full Stack JavaScript Developer.</p>
-                    <SocialLinks />
-                </div>
+            <div className='contact-content-wrapper'>
+                <form>
+                    <fieldset>
+                        <label>Name</label>
+                        <input />
+                    </fieldset>
+                    <fieldset>
+                        <label>Email</label>
+                        <input />
+                    </fieldset>
+                    <fieldset>
+                        <label>Message</label>
+                        <textarea />
+                    </fieldset>
+                    <BaseButton onClick={()=>{}} />
+                </form>
+                <SocialLinks />
             </div>
         </section>
     )
