9c3430f2a902c9b13ce9ea548f4613d53e37f126
Rachel <r.a.reill18@gmail.com>
2021-08-31 17:35:06 -0500

    adds front end service to handle AJAX request

---
+++ client/services/postContactForm.js
@@ -0,0 +1,13 @@
+export async function postContactForm({name, email, message}){
+    try {
+        const response = await fetch(process.env.API_URL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name, email, message})
+        })
+        const json  = await response.json()
+        return json
+    } catch (error) {
+        console.error('Error fetching Medium articles: ', error?.message)
+    }
+}
