710d2deca3db5920c6855e38d8105ed30d62a7b7
Rachel <r.a.reill18@gmail.com>
2021-08-31 09:59:01 -0500

    sets up mail endpoint untested

--- api/mailRouter.js
+++ api/mailRouter.js
@@ -1,21 +1,43 @@
 const express = require('express')
 const mailjet = require ('node-mailjet')
 
-const {getMediumArticles} = require('./getMediumArticles.js')
-
 const mailRouter = express.Router()
+const jsonBodyParser = express.json()
 mailjet.connect(process.env.MAILJET_KEY_1, process.env.MAILJET_KEY_2)
 
 mailRouter
-    .get('/', async (req, res, next) => {
+    .post('/', jsonBodyParser, async (req, res, next) => {
         try {
-            const articles = await getMediumArticles()
-            // FOR TESTING, PAGINATE INSTEAD OF RETURNING ALL ARTICLES!!!
-            const PAGINATE_ME = articles.slice(0, 3)
-            return res
-            .status(200)
-            .set('Access-Control-Allow-Origin', CLIENT_URL)
-            .json(PAGINATE_ME)
+            console.log('THIS IS THE REQ BODY', req.body)
+            const {name, email, message} = {name: 'TEST', email: process.env.EMAIL_TEST, message:'testing, testing mofos'}
+            const request = mailjet
+            .post("send", {'version': 'v3.1'})
+            .request({
+            "Messages":[
+                {
+                "From": {
+                    "Email": email,
+                    "Name": name
+                },
+                "To": [
+                    {
+                    "Email": process.env.EMAIL,
+                    "Name": "Rachel Reilly"
+                    }
+                ],
+                "Subject":  `Rachel, ${name} Reached Out via Your Contact Form`,
+                "TextPart": message,
+                "CustomID": email
+                }
+            ]
+            })
+            request
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
           } catch (error) {
               const errorMessage = 'Error fetching Medium articles: ' + (error?.message || error)
               if (process.env.NODE_ENV === 'development') console.error(errorMessage)
@@ -28,35 +50,3 @@ mailRouter
 
 module.exports = feedRouter
 
-const {name, email, message} = {name: 'TEST', email: process.env.EMAIL_TEST, message:'testing, testing mofos'}
-
-
-
-const request = mailjet
-.post("send", {'version': 'v3.1'})
-.request({
-  "Messages":[
-    {
-      "From": {
-        "Email": email,
-        "Name": name
-      },
-      "To": [
-        {
-          "Email": process.env.EMAIL,
-          "Name": "Rachel Reilly"
-        }
-      ],
-      "Subject":  `Rachel, ${name} Reached Out via Your Contact Form`,
-      "TextPart": message,
-      "CustomID": email
-    }
-  ]
-})
-request
-  .then((result) => {
-    console.log(result.body)
-  })
-  .catch((err) => {
-    console.log(err.statusCode)
-  })
