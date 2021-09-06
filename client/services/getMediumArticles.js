9fce21a40b2d00a38543e3adc71bba15f3933a1f
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-06 13:52:20 -0500

    sets up post request on front end

--- client/services/getMediumArticles.js
+++ client/services/getMediumArticles.js
@@ -1,10 +1,17 @@
-export async function getMediumArticles(){
+export async function getMediumArticles(offset=0){
     try {
-        const response = await fetch(`${process.env.API_URL}/api/feed`, {headers: {'Content-Type': 'application/json'}})
+        const response = await fetch(`${process.env.API_URL}/api/feed`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({offset})
+        })
         const json  = await response.json()
         return json
     } catch (error) {
         console.error('Error fetching Medium articles: ', error?.message)
+        return error
     }
 
 }
