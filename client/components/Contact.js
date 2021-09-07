63de2c5ecc2afcb8f8def3fdb99ace346d08f09d
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-07 17:02:48 -0500

    sets up banner that is not sticky

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -4,7 +4,7 @@ import SocialLinks  from './SocialLinks'
 
 export default function Contact(){
     return (
-        <section className='contact-wrapper '> 
+        <section className='contact-wrapper full-section'> 
             <h2>Contact</h2>
             <div className='contact-content-wrapper'>
                 <ContactForm />
