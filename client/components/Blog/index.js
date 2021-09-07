a4d7629c5df55d99e1477978467f70b16afbe950
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-06 19:05:42 -0500

    adds resume as asset and links in SocialLinks

--- client/components/Blog/index.js
+++ client/components/Blog/index.js
@@ -1,8 +1,6 @@
 import {Fragment} from 'react'
 import BlogPost from './BlogPost'
-import BlogError from './BlogError'
 
-//TODO: Extract error screen to parent component level
 export default function BlogPosts({blogPosts}){
     const hasBlogPosts = blogPosts?.length
     return (
