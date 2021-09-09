9350ad6ef36be6952cb5027b8f3f1c5511df4e4b
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-09 14:10:30 -0500

    adds error message to indicate form is inactive

--- client/services/postContactForm.js
+++ client/services/postContactForm.js
@@ -9,11 +9,14 @@ export async function postContactForm({name, email, message}){
             body: JSON.stringify({name, email, message})
         })
         console.log('THIS ACTUALLY GOT A RESPONSE')
-        const json  = response.json()
+        const json  = await response.json()
         console.log('THIS IS THE JSON RESPONSE')
         return json
     } catch (error) {
+        console.log('THIS DID NOT GO THROUGH!!!!!!')
         console.error('Error sending message: ', error?.message)
         return error
+    } finally {
+        console.log('THIS IS THE ENDDDDDDD')
     }
 }
