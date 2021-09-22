78a04bff5244723e92ca0ccc4206afd5d605fd16
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-22 17:57:10 -0500

    removes unused imports from root screen

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,7 @@
-import { useEffect } from 'react'
-import Header from '../components/Header'
 import Banner from '../components/Banner/index'
 import Layout from '../components/layout'
-import { getMediumArticles } from '../services/getMediumArticles'
 
 export default function Home() {
-//TODO: fetch articles from API, sanitize, set in state
   return (
     <Layout>
       <Banner />
