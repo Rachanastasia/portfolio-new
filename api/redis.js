f19970c185eaf012a5c1c7ee5e5c661eeb24619c
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-25 13:12:28 -0500

    sets up basic queue for mail

--- api/redis.js
+++ api/redis.js
@@ -1,13 +1,24 @@
 const redis = require('redis')
+const Queue = require('bull')
 const {getMediumArticles} = require('./getMediumArticles.js')
 
+const BLOG = 'blog_posts'
+const MAIL = 'send_mail'
+
 const client = redis.createClient(6379, '127.0.0.1')
+const mailQueue = new Queue(MAIL, { redis: { port: 6379, host: '127.0.0.1'} })
 
 client.on('error', error => {
     console.error('Error ' + error)
 })
 
-const BLOG = 'blog_posts'
+mailQueue.process(function (job, done){
+    job.progress(42)
+    console.log('PRENTENDING TO PROCESS')
+    done()
+    throw new Error('Could not process mail')
+})
+
 
 async function handleBlogCache(){
     if (!client.get(BLOG, redis.print)){
