30299928eaee8884d6edf9582a80174f60db7954
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-25 13:34:09 -0500

    removes unused heroku files

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
-const redis = require('redis')
 
 const { PORT } = require('./config')
 const feedRouter = require('./feedRouter')
