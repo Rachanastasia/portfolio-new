1e20b3e4c0a2b12e74abd054a0b6861323b4ab4c
Rachel Reilly <r.a.reill18@gmail.com>
2021-10-08 18:52:10 -0500

    updates resume and fixes link to file

--- client/components/SocialLinks.js
+++ client/components/SocialLinks.js
@@ -1,25 +1,28 @@
-import {RiLinkedinLine, RiGithubLine} from 'react-icons/ri'
-import {AiOutlineMedium, AiOutlineFile} from 'react-icons/ai'
+import { RiLinkedinLine, RiGithubLine } from "react-icons/ri";
+import { AiOutlineMedium, AiOutlineFile } from "react-icons/ai";
 
-
-export default function SocialLinks({hasResume=false, className}){
-    const wrapperClass = className + ' about-links-wrapper' 
-   return (
+export default function SocialLinks({ hasResume = false, className }) {
+  const wrapperClass = className + " about-links-wrapper";
+  return (
     <div className={wrapperClass}>
-        <a className='social-icon' href='https://www.linkedin.com/in/rachelrly/'>
-            <RiLinkedinLine />
+      <a className="social-icon" href="https://www.linkedin.com/in/rachelrly/">
+        <RiLinkedinLine />
+      </a>
+      <a className="social-icon" href="https://rachelrly.medium.com/">
+        <AiOutlineMedium />
+      </a>
+      <a className="social-icon" href="https://github.com/rachelrly">
+        <RiGithubLine />
+      </a>
+      {hasResume && (
+        <a
+          className="social-icon"
+          href={`${process.env.CLIENT_URL}/assets/RachelReillyResume.pdf`}
+          download="RachelReillyResume"
+        >
+          <AiOutlineFile />
         </a>
-        <a className="social-icon" href='https://rachelrly.medium.com/'>
-            <AiOutlineMedium/>
-        </a>
-        <a className='social-icon' href='https://github.com/rachelrly'>
-            <RiGithubLine />
-        </a>
-        {hasResume && (
-        <a className='social-icon' href='../assets/RachelReillyResume.pdf'>
-            <AiOutlineFile />
-        </a>)}
+      )}
     </div>
-    )
+  );
 }
-
--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    env: {
-      API_URL: process.env.NODE_ENV === 'development' 
-      ? process.env.DEV_API_URL 
-      : process.env.API_URL
-    },
-  }
+  env: {
+    API_URL:
+      process.env.NODE_ENV === "development"
+        ? process.env.DEV_API_URL
+        : process.env.API_URL,
+    CLIENT_URL: "https://portfolio-staging-rachanastasia.vercel.app/",
+  },
+};
