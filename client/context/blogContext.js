4fe09da1c271f464c9cf5ab6c0f313dff51bdfb7
Rachel Reilly <r.a.reill18@gmail.com>
2021-10-19 18:08:56 -0500

    removes iefe and fixes request params

--- client/context/blogContext.js
+++ client/context/blogContext.js
@@ -1,23 +1,25 @@
-import { useEffect, useState, createContext } from "react"
-import { getMediumArticles } from '../services/getMediumArticles'
+import { useEffect, useState, createContext } from "react";
+import { getMediumArticles } from "../services/getMediumArticles";
 
-export const BlogContext = createContext()
+export const BlogContext = createContext();
 
-export default function BlogContextProvider({children}){
-    const [blogPosts, setBlogPosts] = useState([])
+export default function BlogContextProvider({ children }) {
+  const [blogPosts, setBlogPosts] = useState([]);
 
-    useEffect(()=>{
-        (async function(){
-            try{
-                const posts = await getMediumArticles()
-                setBlogPosts(posts)
-            } catch (error) {
-                console.error('Error fetching blog posts', error?.message)
-            }
-        })()
-    },[])
+  useEffect(() => {
+    if (!children?.length) fetchMediumArticles();
+  }, []);
 
-    const value = {blogPosts}
+  async function fetchMediumArticles() {
+    try {
+      const posts = await getMediumArticles();
+      if (blogPosts !== posts) setBlogPosts(posts);
+    } catch (error) {
+      console.error("Error fetching blog posts", error?.message);
+    }
+  }
 
-    return <BlogContext.Provider value={value}>{children}</BlogContext.Provider>
+  const value = { blogPosts };
+
+  return <BlogContext.Provider value={value}>{children}</BlogContext.Provider>;
 }
--- client/services/getMediumArticles.js
+++ client/services/getMediumArticles.js
@@ -1,16 +1,9 @@
-export async function getMediumArticles(offset=0){
-    try {
-        const response = await fetch(`${process.env.API_URL}/api/feed`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({offset})
-        })
-        const json  = await response.json()
-        return json
-    } catch (error) {
-        console.error('Error fetching Medium articles: ', error?.message)
-    }
-
+export async function getMediumArticles(offset = 0) {
+  try {
+    const response = await fetch(`${process.env.API_URL}/api/feed`);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error("Error fetching Medium articles: ", error?.message);
+  }
 }
