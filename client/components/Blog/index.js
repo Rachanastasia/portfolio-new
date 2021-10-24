9c89a2e929a134f13093e212980926bb4b6e3aac
Rachel Reilly <r.a.reill18@gmail.com>
2021-10-24 16:48:08 -0500

    style improvements

--- client/components/Blog/index.js
+++ client/components/Blog/index.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
-import BlogError from "./BlogError";
 import BlogPost from "./BlogPost";
 
 export default function BlogPosts({ blogPosts }) {
   return (
     <Fragment>
-      {blogPosts?.length &&
-        blogPosts.map((post) => <BlogPost {...post} key={post.datePosted} />)}
+      {blogPosts?.length ? (
+        blogPosts.map((post) => <BlogPost {...post} key={post.datePosted} />)
+      ) : (
+        <Fragment />
+      )}
     </Fragment>
   );
 }
--- client/components/Projects/ProjectCard.js
+++ client/components/Projects/ProjectCard.js
@@ -1,25 +1,41 @@
 /**the "Landscape" in ProjectCardLandscape refers to the orientation of the project's image*/
-import Image from 'next/image'
-import Links from './Links'
-import Stack from './Stack'
+import Image from "next/image";
+import Links from "./Links";
+import Stack from "./Stack";
 
-export default function ProjectCard({title, image, description, stack, links, imageOptions={}}){
-    title = title.toUpperCase()
-    return (
-        <section className='project-wrapper'>
-            <div className='title-stack-wrapper'>
-                <h3 className='section-title'>{title}</h3>
-                <Stack stack={stack} />
-            </div>
-            <div className='project-content-wrapper'>
-                <div className='inner-content-wrapper'>
-                <div className='image-link-wrapper'>
-                    <Image src={image} {...imageOptions} className='project-image project-image-landscape test-image-class' layout='fixed' />
-                </div>
-                <Links  links={links} className='lg-vertical' buttonClassName='project-link'/>           
-                </div>
-                <p>{description}</p>
-            </div>
-        </section>
-    )
+export default function ProjectCard({
+  title,
+  image,
+  description,
+  stack,
+  links,
+  imageOptions = {},
+}) {
+  title = title.toUpperCase();
+  return (
+    <section className="project-wrapper">
+      <div className="title-stack-wrapper">
+        <h3>{title}</h3>
+        <Stack stack={stack} />
+      </div>
+      <div className="project-content-wrapper">
+        <div className="inner-content-wrapper">
+          <div className="image-link-wrapper">
+            <Image
+              src={image}
+              {...imageOptions}
+              className="project-image project-image-landscape test-image-class"
+              layout="fixed"
+            />
+          </div>
+          <Links
+            links={links}
+            className="lg-vertical"
+            buttonClassName="project-link"
+          />
+        </div>
+        <p>{description}</p>
+      </div>
+    </section>
+  );
 }
