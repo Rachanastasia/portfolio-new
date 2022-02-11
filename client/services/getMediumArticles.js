6eab90f747c75e7e3596c0d0a927069d9cb1e80e
Rachel <rachelreillydev@gmail.com>
2022-02-11 15:43:40 -0600

    sets up Svelte project with TypeScript and Tailwind

--- client/services/getMediumArticles.js
+++
@@ -1,9 +0,0 @@
-export async function getMediumArticles() {
-  try {
-    const response = await fetch(`${process.env.API_URL}/api/feed`);
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    console.error("Error fetching Medium articles: ", error?.message);
-  }
-}
