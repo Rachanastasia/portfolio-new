5d6a383cfc546729515f07806686353fd4675527
Rachel <rachelreillydev@gmail.com>
2022-02-11 16:46:13 -0600

    changes app to sveltekit

---
+++ app/src/lib/Image.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let src: any
+  export let alt: string
+</script>
+
+<div>
+  <picture>
+    <source srcset={src} type="image/jpg" />
+    <img {src} {alt} />
+  </picture>
+</div>
