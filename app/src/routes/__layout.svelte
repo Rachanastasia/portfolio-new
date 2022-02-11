dc00e7a1dc6b7c78cdf99950c792668beee213fe
Rachel <rachelreillydev@gmail.com>
2022-02-11 16:46:13 -0600

    changes app to sveltekit

---
+++ app/src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<main class="flex items-center h-screen w-screen">
+  <slot />
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ app/src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+  import Image from '../lib/Image.svelte'
+  import me from '../../static/rachel_reilly.jpg'
+  export const prerender = true
+</script>
+
+<svelte:head>
+  <title>Rachel Reilly | Software Engineer</title>
+</svelte:head>
+
+<div class="bg-red-400">
+  <Image alt="testing" src={me} />
+</div>
