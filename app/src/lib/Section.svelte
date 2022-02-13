dcb1e78029b126acd86e12c81cdc59911ea1d1e5
Rachel <rachelreillydev@gmail.com>
2022-02-13 17:22:29 -0600

    adds backgrounds to sections

--- app/src/lib/Section.svelte
+++ app/src/lib/Section.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  export let color: string
+  export let bg: string
 </script>
 
-<section
-  class="flex-none flex flex-col items-center justify-center h-screen w-full px-2 {color}"
->
-  <slot />
+<section class="flex-none flex items-center justify-center h-96 w-full {bg}">
+  <div class="max-w-4xl flex flex-col items-center justify-center  p-3">
+    <slot />
+  </div>
 </section>
---
+++ app/src/lib/Title.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-3xl">Rachel Reilly</h1>
